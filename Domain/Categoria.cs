a26a725bbd7a36e6978213b01a062291603310e8
Breno Marcomini <brenomarcomini@hotmail.com>
2022-09-26 23:14:10 -0300

    Relacionamento entre Tabelas

--- Domain/Categoria.cs
+++ Domain/Categoria.cs
@@ -1,11 +1,18 @@
-﻿namespace APICatalog.Domain;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalog.Domain;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int CategoriaId { get; set; }
     
     public string? Nome { get; set; }
     
     public string? ImagemUrl { get; set; }
     
+    public ICollection<Produto>? Produtos { get; set; }
 }
